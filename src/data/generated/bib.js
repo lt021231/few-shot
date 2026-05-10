1e6fb8b55cb222f42fc08ec1ff0c477f37cd23fe
lt021231 <91208430+lt021231@users.noreply.github.com>
2026-05-10 19:23:37 +0800

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,174 @@
+﻿const generatedBibEntries = {
+    "Chen2019CloserLook": {
+        "author": "Chen, Wei-Yu and Liu, Yen-Cheng and Kira, Zsolt and Wang, Yu-Chiang Frank and Huang, Jia-Bin",
+        "booktitle": "Proceedings of the 7th International Conference on Learning Representations (ICLR)",
+        "doi": "10.48550/arXiv.1904.04232",
+        "keywords": "baseline-analysis, feature-space, cross-domain, few-shot-learning",
+        "title": "A Closer Look at Few-shot Classification",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Chen2020SimCLR": {
+        "author": "Chen, Ting and Kornblith, Simon and Norouzi, Mohammad and Hinton, Geoffrey",
+        "booktitle": "Proceedings of the 37th International Conference on Machine Learning (ICML)",
+        "doi": "10.48550/arXiv.2002.05709",
+        "keywords": "self-supervised-learning, contrastive-learning, representation-learning",
+        "pages": "1597--1607",
+        "title": "A Simple Framework for Contrastive Learning of Visual Representations",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Ding2020GraphProto": {
+        "author": "Ding, Kaize and Wang, Jianling and Li, Jundong and Shu, Kai and Liu, Chenghao and Liu, Huan",
+        "booktitle": "Proceedings of the 29th ACM International Conference on Information \\& Knowledge Management (CIKM)",
+        "doi": "10.1145/3340531.3411922",
+        "keywords": "prototype, graph-learning, few-shot-learning",
+        "pages": "295--304",
+        "title": "Graph Prototypical Networks for Few-shot Learning on Attributed Networks",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Finn2017MAML": {
+        "author": "Finn, Chelsea and Abbeel, Pieter and Levine, Sergey",
+        "booktitle": "Proceedings of the 34th International Conference on Machine Learning (ICML)",
+        "doi": "10.48550/arXiv.1703.03400",
+        "keywords": "meta-learning, optimization-based, few-shot-learning",
+        "pages": "1126--1135",
+        "title": "Model-Agnostic Meta-Learning for Fast Adaptation of Deep Networks",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "Gharoun2024Survey": {
+        "author": "Gharoun, Hassan and Momenifar, Fereshteh and Chen, Fang and Gandomi, Amir H.",
+        "doi": "10.1145/3659943",
+        "journal": "ACM Computing Surveys",
+        "keywords": "survey, meta-learning, few-shot-learning",
+        "number": "12",
+        "pages": "Article 294",
+        "title": "Meta-learning Approaches for Few-Shot Learning: A Survey of Recent Advances",
+        "type": "article",
+        "volume": "56",
+        "year": "2024"
+    },
+    "He2020MoCo": {
+        "author": "He, Kaiming and Fan, Haoqi and Wu, Yuxin and Xie, Saining and Girshick, Ross",
+        "booktitle": "Proceedings of the 2020 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.48550/arXiv.1911.05722",
+        "keywords": "self-supervised-learning, contrastive-learning, representation-learning",
+        "pages": "9726--9735",
+        "title": "Momentum Contrast for Unsupervised Visual Representation Learning",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Ji2020ImprovedProto": {
+        "author": "Ji, Zhong and Chai, Xingliang and Yu, Yunlong and Pang, Yanwei and Zhang, Zhongfei",
+        "doi": "10.1016/j.patrec.2020.07.015",
+        "journal": "Pattern Recognition Letters",
+        "keywords": "prototype, adaptive-representation, few-shot-learning",
+        "pages": "144--150",
+        "title": "Improved Prototypical Networks for Few-Shot Learning",
+        "type": "article",
+        "volume": "130",
+        "year": "2020"
+    },
+    "Koch2015Siamese": {
+        "author": "Koch, Gregory and Zemel, Richard and Salakhutdinov, Ruslan",
+        "booktitle": "ICML Deep Learning Workshop",
+        "doi": "10.48550/arXiv.1503.02531",
+        "keywords": "historical-foundation, metric-based, siamese-network",
+        "title": "Siamese Neural Networks for One-Shot Learning",
+        "type": "inproceedings",
+        "volume": "37",
+        "year": "2015"
+    },
+    "Oreshkin2018TaskAdaptive": {
+        "author": "Oreshkin, Boris and Rodr\u00edguez, Pau and Lacoste, Alexandre",
+        "booktitle": "Advances in Neural Information Processing Systems (NeurIPS)",
+        "doi": "10.48550/arXiv.1805.10123",
+        "keywords": "metric-based, task-adaptive-metric, few-shot-learning",
+        "pages": "6438--6448",
+        "title": "Task Dependent Adaptive Metric for Improved Few-Shot Learning",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "Schroff2015FaceNet": {
+        "author": "Schroff, Florian and Kalenichenko, Dmitry and Philbin, James",
+        "booktitle": "Proceedings of the 2015 IEEE Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2015.7298682",
+        "keywords": "historical-foundation, metric-based, triplet-loss, embedding",
+        "pages": "815--823",
+        "title": "FaceNet: A Unified Embedding for Face Recognition and Clustering",
+        "type": "inproceedings",
+        "year": "2015"
+    },
+    "Schwartz2019Manifold": {
+        "author": "Schwartz, Eli and Karlinsky, Leonid and Feris, Rog\u00e9rio and Giryes, Raja and Bronstein, Alexander M.",
+        "booktitle": "Proceedings of the 2019 IEEE/CVF International Conference on Computer Vision (ICCV)",
+        "doi": "10.48550/arXiv.1907.12087",
+        "keywords": "adaptive-representation, feature-space, regularization, few-shot-learning",
+        "pages": "2500--2509",
+        "title": "Charting the Right Manifold: Manifold Mixup for Few-Shot Learning",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Snell2017ProtoNet": {
+        "author": "Snell, Jake and Swersky, Kevin and Zemel, Richard S.",
+        "booktitle": "Advances in Neural Information Processing Systems (NeurIPS)",
+        "doi": "10.48550/arXiv.1703.05175",
+        "keywords": "metric-based, prototype, few-shot-learning",
+        "pages": "4080--4090",
+        "title": "Prototypical Networks for Few-shot Learning",
+        "type": "inproceedings",
+        "year": "2017"
+    },
+    "Sung2018RelationNet": {
+        "author": "Sung, Flood and Yang, Yongxin and Zhang, Li and Xiang, Tao and Torr, Philip H. S. and Hospedales, Timothy M.",
+        "booktitle": "Proceedings of the 2018 IEEE/CVF Conference on Computer Vision and Pattern Recognition (CVPR)",
+        "doi": "10.1109/CVPR.2018.00131",
+        "keywords": "metric-based, relation-metric, few-shot-learning",
+        "pages": "1199--1208",
+        "title": "Learning to Compare: Relation Network for Few-Shot Learning",
+        "type": "inproceedings",
+        "year": "2018"
+    },
+    "Vinyals2016MatchingNet": {
+        "author": "Vinyals, Oriol and Blundell, Charles and Lillicrap, Timothy and Kavukcuoglu, Koray and Wierstra, Daan",
+        "booktitle": "Advances in Neural Information Processing Systems (NeurIPS)",
+        "doi": "10.48550/arXiv.1606.04080",
+        "keywords": "metric-based, matching, few-shot-learning",
+        "pages": "3630--3638",
+        "title": "Matching Networks for One Shot Learning",
+        "type": "inproceedings",
+        "year": "2016"
+    },
+    "Wang2019SimpleShot": {
+        "author": "Wang, Yan and Chao, Wei-Lun and Weinberger, Kilian Q. and van der Maaten, Laurens",
+        "booktitle": "Advances in Neural Information Processing Systems (NeurIPS)",
+        "doi": "10.48550/arXiv.1911.04623",
+        "keywords": "baseline-analysis, feature-space, nearest-neighbor, few-shot-learning",
+        "pages": "11128--11137",
+        "title": "SimpleShot: Revisiting Nearest-Neighbor Classification for Few-Shot Learning",
+        "type": "inproceedings",
+        "year": "2019"
+    },
+    "Wang2020ProtoSiamese": {
+        "author": "Wang, Junhua and Zhai, Yongping",
+        "booktitle": "2020 IEEE 10th International Conference on Electronics Information and Emergency Communication (ICEIEC)",
+        "doi": "10.1109/ICEIEC49280.2020.9152261",
+        "keywords": "metric-based, prototype, siamese-network, few-shot-learning",
+        "pages": "1--6",
+        "title": "Prototypical Siamese Networks for Few-shot Learning",
+        "type": "inproceedings",
+        "year": "2020"
+    },
+    "Ye2020FEAT": {
+        "author": "Ye, Han-Jia and Hu, Hexiang and Zhan, De-Chuan and Sha, Fei",
+        "booktitle": "Proceedings of the 37th International Conference on Machine Learning (ICML)",
+        "doi": "10.48550/arXiv.1812.03664",
+        "keywords": "adaptive-representation, embedding-adaptation, few-shot-learning",
+        "pages": "10617--10626",
+        "title": "FEAT: Few-Shot Embedding Adaptation with Set-to-Set Functions",
+        "type": "inproceedings",
+        "year": "2020"
+    }
+};
