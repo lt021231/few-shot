f9637fe110534d27fb968ef3b45bd8ce8a1eb059
lt021231 <91208430+lt021231@users.noreply.github.com>
2026-05-12 16:36:38 +0800

    Update tag_categories.js

--- src/data/tag_categories.js
+++ src/data/tag_categories.js
@@ -1,5 +1,36 @@
-const userDefinedTagCategories = {
-	"type": {
-		"description": "type of the paper"
-	}
-}
+var tagCategories = {
+    "Method": [
+        "metric-based",
+        "meta-learning",
+        "optimization-based",
+        "self-supervised-learning",
+        "contrastive-learning",
+        "adaptive-representation",
+        "regularization"
+    ],
+    "Architecture": [
+        "siamese-network",
+        "prototype",
+        "matching",
+        "relation-metric",
+        "embedding-adaptation",
+        "task-adaptive-metric"
+    ],
+    "Analysis": [
+        "baseline-analysis",
+        "feature-space",
+        "cross-domain",
+        "nearest-neighbor",
+        "representation-learning"
+    ],
+    "Application": [
+        "few-shot-learning",
+        "graph-learning",
+        "embedding",
+        "triplet-loss"
+    ],
+    "Type": [
+        "historical-foundation",
+        "survey"
+    ]
+};
